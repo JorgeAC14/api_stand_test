a084f5f722288bbf6708839af232762f375d2759
Jorge Albicker <jorgealbicker@Jorge-Albicker.local>
2024-03-19 09:36:57 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,5 @@
+URL_SERVICE = "https://cnt-fc3d5075-7af6-4c5c-bd1c-b46e1364265e.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
